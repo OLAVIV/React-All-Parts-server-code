6b2681218ae88bf7fbe2e7391a02e7e5619d6812
olaVI <olavi.kurki2004@gmail.com>
2022-04-07 22:44:41 +0300

    mongo

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.delete("/api/reminders/:id", function (req, res) {
   MongoClient.connect(dbConnection, function (err, client) {
     var db = client.db('Reminders');
     db.collection('Reminder')
-      .findOne({ _id: reminderId }, reminder => {
+      .find({ _id: reminderId }, reminder => {
         console.log(reminder)
         if (!reminder) {
           res.status(406).send("Reminder doesn't exist!");
